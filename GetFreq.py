aaf4be4254d1d2ec7b69e864b5a22ac428fd62cc
amuroni <muronialessandro@gmail.com>
2019-02-17 09:51:34 +0100

    Tested a search in the dataframe, avoiding all empty dataframe. Works!

--- GetFreq.py
+++ GetFreq.py
@@ -24,17 +24,21 @@ urls_list = ("https://diretta.frequence-radio.com/frequenza-radio-abruzzo.html",
              "https://diretta.frequence-radio.com/frequenza-radio-valle-daosta.html",
              "https://diretta.frequence-radio.com/frequenza-radio-veneto.html")
 
-# we load the url list from which we will scrape the data all at once instead of doing it for every single url
+# load url list to scrape data all at once, not for every single url
 
-writer = pd.ExcelWriter("Frequencytable.xlsx", engine="xlsxwriter")  # xslx name and the engine for df.to_excel
+# writer = pd.ExcelWriter("Frequencytable.xlsx", engine="xlsxwriter")  # xslx name and the engine for df.to_excel
 
-row = 0                                               # row starts at 0, see below
+# row = 0                                               # row starts at 0, see below
 for url in urls_list:
     page = requests.get(url)                          # get data from each url
     soup = BeautifulSoup(page.content, "lxml")        # let BSoup load the raw html with html.parser
     table = soup.findAll("table")[0]                  # get the tables from the htlm raw
     dfs = pd.read_html(str(table))                    # read the str items in the tables; contains ALL tables.
-    df = dfs[0]                                       # generate the dataframe to use for .to_excel pandas method
-    df.to_excel(writer, "Frequencies", startrow=row)  # print the dataframe into the excel sheet
-    row = row + len(df.index) + 1                     # starting row for each df: len(df.index) = n. rows of each df)
-writer.save()                                         # close the .to_excel by saving
+    df = dfs[0]
+    df2 = df.loc[(df["Province"] == "Cagliari")]      # made a search in the dataframe for Province
+    if not df2.empty:                                 # avoid empty df (province != selected one)
+        print(df2)                                    # print only df with values
+#     df.to_excel(writer, "Frequencies", startrow=row)  # print the dataframe into the excel sheet
+#     row = row + len(df.index) + 1                     # starting row for each df: len(df.index) = n. rows of each df)
+# writer.save()                                         # close the .to_excel by saving
+
